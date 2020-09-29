280ff7791b6ce744f5f9a5d1dfa24d9d6188c1db
NCSaketh <nc.saketh1@gmail.com>
2020-09-30 01:37:36 +0530

    UC8 : Computing Employee Wage for multiple companies

--- src/EmpWageBuilderUC1.java
+++ src/EmpWageBuilderUC1.java
@@ -1,39 +1,48 @@
 public class EmpWageBuilderUC1 {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
+	
+	/*public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 2;
 	public static final int MAX_HRS_IN_MONTH = 100;
-
-
-	public static void main(String[] args) {
-		System.out.println("Total Emp Wage: " + compute() );
-	}	
-		
-public static int compute()
-{
+	*/
+			
+ public static int compute(String company ,int EMP_RATE_PER_HOUR,int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+ {
 	int empHrs = 0, empWage = 0, totalEmpWage = 0;
 	for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
 	{
-	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	switch (empCheck)
-	{
-	case IS_PART_TIME:
-	empHrs = 4;
-	break;
-	 case IS_FULL_TIME:
+	 int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 	
-	empHrs = 8;
-	break;
-	default:
-	empHrs = 0;
+	 switch (empCheck)
+	 {
+	 case IS_PART_TIME:
+	 empHrs = 4;
+	 break;
+	
+	 case IS_FULL_TIME:			
+	 empHrs = 8;
+	 break;
+	
+	 default:
+	 empHrs = 0;
+	
 	}
+	 
 	empWage = empHrs * EMP_RATE_PER_HOUR;
 	totalEmpWage += empWage;
-	System.out.println("Emp Wage: " + empWage);
+	//System.out.println("Emp Wage: " + empWage);
 	}
 	
 	return totalEmpWage;
 
-}
+ }
+
+ public static void main(String[] args) 
+ {
+	compute("Tesla",20, 2, 100 );
+	compute("Jio",10, 4, 100 ); 
+ }	
+
+
 }
